48aeb9fd015020096130f409be2b6dcb4c776d97
yanglidong <aiyld@hotmail.com>
2018-12-13 17:35:58 +0800

    feat: 修改为不依赖vue及vue loader

--- src/web/index.js
+++ src/web/index.js
@@ -1,13 +1,19 @@
-import Vue from 'vue'
-import App from './App'
-import BootstrapVue from 'bootstrap-vue'
+// import Vue from 'vue'
+// import App from './App'
+// import BootstrapVue from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-Vue.use(BootstrapVue);
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+// Vue.use(BootstrapVue);
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
+
+import originLocales from "./locale";
+const langs = Object.keys(originLocales);
+window.langs = langs;
+window.originLocales = JSON.parse(JSON.stringify(originLocales));
+window.langBase = LANG_BASE;
