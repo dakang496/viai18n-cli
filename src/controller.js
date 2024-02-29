e8a6ce40d6e67e075d051c5601b687ea48b22881
dakang496 <1208303205@qq.com>
2024-02-29 14:46:02 +0800

    feat: 找回旧版遗漏代码

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,6 @@
 const helper = require('./helper');
+const shell = require('shelljs');
+const Path = require('path');
 
 function Controller(options) {
   this.options = options;
@@ -33,15 +35,55 @@ Controller.prototype.start = async function () {
   this.runAfter();
 }
 
+Controller.prototype.highlight = function (text, color = "green") {
+  if (color === "red") {
+    return "\033[31m" + text + "\033[0m";
+  }
+  return "\033[32m" + text + "\033[0m";
+}
+
+Controller.prototype.getIncludedFiles = function () {
+  const options = this.options;
+  const gitConfig = options.include && options.include.git;
+  const baseBranch = gitConfig && gitConfig.baseBranch;
+  if (!baseBranch) {
+    return null;
+  }
+  console.log(this.highlight("\nCurrent branch:"));
+  const currentBranch = shell.exec("git branch --show-current").stdout.trim();
+  if (currentBranch === baseBranch) {
+    console.log(this.highlight(`\nCurrent branch is ${currentBranch} which is same as the base branch, so this 'include' feature doesn't work!!!`, "red"));
+    return null;
+  }
+  console.log(this.highlight(`Only collect modified files based on ${baseBranch} branch!!!`, "red"));
+  const postfix = options.resolve.postfix
+  console.log(this.highlight("Current branch first hash:"));
+  const firstHash = shell.exec(`git log ${baseBranch}..HEAD --pretty=format:%h | tail -1`).stdout.trim();
+  console.log(this.highlight("\nIt previous hash:"));
+  const previousHash = shell.exec(`git rev-parse --short ${firstHash}^1`).stdout.trim()
+  console.log(this.highlight("Modified files:"));
+  const files = shell.exec(`git diff --name-only ${previousHash} -- '**/*${postfix}'`).stdout.trim().split("\n");
+
+  return files.map((file) => {
+    return Path.resolve(file);
+  });
+}
+
+
 Controller.prototype.run = async function () {
   const entry = this.options.entry;
   const fileRegx = this.options._fileRegx;
   const filePattern = this.options.exclude.filePattern;
 
+  const includedFiles = this.getIncludedFiles();
+
 
   Object.keys(entry).forEach((name) => {
     const rootPath = entry[name];
     helper.traverse(rootPath, (filePath, originContent) => {
+      if (includedFiles && !includedFiles.includes(filePath)) {
+        return;
+      }
       originContent = JSON.parse(originContent);
       let content = originContent
       this.resolveFilters.forEach((filter) => {
